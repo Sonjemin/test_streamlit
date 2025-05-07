637814a844c2326bc5931adc13f35d8c35fe85b6
Sonjemin <jemin68003726@gmail.com>
2025-05-07 14:44:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from openai import OpenAI
 
 api_key= st.text_input("OpenAI API Key", type="password")
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key="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")
 
 st.title("OpenAI GPT model")
 
